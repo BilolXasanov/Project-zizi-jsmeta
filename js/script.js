be54f28eb93988126ab43c49f0086d04294eda31
BilolXasanov <xspm24@gmail.com>
2025-01-17 09:07:56 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+const menuBtn = document.querySelector('.menu-btn'),
+      navigation = document.querySelector(".nav-menu");
+
+      menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('active');
+        navigation.classList.toggle('active');
+      });
