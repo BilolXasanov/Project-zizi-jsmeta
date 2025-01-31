098763d411426d4d81b579cc12653291f08f2f8e
BilolXasanov <xspm24@gmail.com>
2025-01-31 08:36:09 +0500

    two

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 
 
+
+
 const menuBtn = document.querySelector('.menu-btn'),
       navigation = document.querySelector(".nav-menu");
+      body = document.body;
 
       menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('active');
         navigation.classList.toggle('active');
+
+        if (navigation.classList.contains('active')) {
+          body.style.overflow = 'hidden';      
+        } else {
+          body.style.overflow = 'auto'
+        }
+        
       });
